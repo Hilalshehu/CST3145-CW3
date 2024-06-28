856b924add0633c1b17425b779c26d3c8a2c656b
hilalshehu <idriiswadaji@gmail.com>
2024-06-29 00:43:11 +0400

    ssss

--- src/main.js
+++ src/main.js
@@ -5,8 +5,7 @@ import App from './App.vue';
 import '../public/bootstrap.css'
 import '../public/bootstrap.min.css';
 
-// Import Font Awesome CSS
-//import '@fortawesome/fontawesome-free/css/all.min.css';
+
 
 import '../public/bootstrap.css'
 import '../public/style.css'
