874b184939f2c0e7b6f515dd2f2da36003dce258
hilalshehu <idriiswadaji@gmail.com>
2024-06-29 00:36:40 +0400

    Deleted Router

--- src/main.js
+++ src/main.js
@@ -1,7 +1,6 @@
 // src/main.js
 import { createApp } from 'vue';
 import App from './App.vue';
-import router from './router';
 // Import Bootstrap CSS
 import '../public/bootstrap.css'
 import '../public/bootstrap.min.css';
@@ -12,5 +11,4 @@ import '../public/bootstrap.min.css';
 import '../public/bootstrap.css'
 import '../public/style.css'
 const app = createApp(App);
-  app.use(router)
   app.mount('#app');
