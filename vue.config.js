317f33a47cf6688e935178663b67dced14af3c9b
hilalshehu <idriiswadaji@gmail.com>
2024-06-30 11:40:21 +0400

    Vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,33 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true,
-  
-})
+
+module.exports = {
+  pwa: {
+    name: 'My App',
+    themeColor: '#4DBA87',
+    msTileColor: '#000000',
+    appleMobileWebAppCapable: 'yes',
+    appleMobileWebAppStatusBarStyle: 'black',
+    manifestOptions: {
+      start_url: '/',
+      display: 'standalone',
+      background_color: '#ffffff'
+    },
+    workboxOptions: {
+      // Workbox options here, if needed
+    }
+  },
+  devServer: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8080',
+        changeOrigin: true,
+        pathRewrite: {
+          '^/api': ''
+        }
+      },
+      '/ws': {
+        target: 'ws://localhost:8080',
+        ws: true
+      }
+    }
+  }
+}
