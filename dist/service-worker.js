0ed54317b6581b15e5c0ad65cfacaa2103302a3b
hilalshehu <idriiswadaji@gmail.com>
2024-06-30 11:34:00 +0400

    mORE Changes

--- dist/service-worker.js
+++ dist/service-worker.js
@@ -12,43 +12,28 @@ self.addEventListener('install', (e) => {
     e.waitUntil(
         caches.open(CacheName).then((cache) => {
             console.log('[Service Worker] Caching all the Files');
-            return cache.addAll(CacheFiles);
+            return Promise.all(
+                CacheFiles.map((file)=>{
+                    return cache.add(file).catch((err)=>{
+                        console.error(`failed to cache ${file}: ${err}`);
+                    });
+                })
+            )
         })
     );
 });
-
 
 self.addEventListener('fetch', function (e) {
     e.respondWith(
-        caches.match(e.request).then(function (r) {
-            if (r) {
-                return r; // Return the cached response if available
-            } else {
-                // Fetch the request from the network
-                return fetch(e.request).then(function (response) {
-                    if (!response || response.status !== 200 || response.type !== 'basic') {
-                        return response; // Do not cache responses that are not successful or not basic
-                    }
-                    var responseToCache = response.clone();
-                    caches.open(CacheName).then(function (cache) {
-                        cache.put(e.request, responseToCache).then(function () {
-                            console.log('[Service Worker] Cached response:', e.request.url);
-                        }).catch(function (error) {
-                            console.error('[Service Worker] Cache put error:', error);
-                        });
-                    });
-                    return response; // Return the fetched response to the fetch event
-                }).catch(function (error) {
-                    console.error('[Service Worker] Fetch failed:', error);
-                    throw error;
+        caches.match(e.request).then(function (r){
+            return r || fetch(e.request).then(function (response){
+                return caches.open(CacheName).then(function (cache){
+                    cache.put(e.request, response.clone());
+                    return response;
                 });
-            }
-        }).catch(function (error) {
-            console.error('[Service Worker] Cache match failed:', error);
-            throw error;
+            }).catch(function (){
+                return caches.match('offline.html');
+            })
         })
-    );
+    )
 });
-
-
-
--- src/App.vue
+++ src/App.vue
@@ -20,6 +20,7 @@ export default {
   components: {
     ProductList,
     CheckoutPage,
+    
   },
   data() {
     return {
